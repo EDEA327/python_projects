b5133480f60dadf621bfeb9df7c6c67a00ce2dd3
Erick <edeajob27@gmail.com>
2023-04-19 11:49:17 -0500

    Dibujando la serpiente

--- snake_game/main.py
+++ snake_game/main.py
@@ -5,4 +5,21 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+
+# Crete the  initial snake
+# # Drawing white squares
+turtles = []
+for _ in range(3):
+    t = Turtle(shape="square")
+    t.color("white")
+    turtles.append(t)
+
+# # Placing the squares next to each other
+turtles[1].goto(x=-20, y=0)
+turtles[2].goto(x=-40, y=0)
+
+# Keep the window open
 screen.exitonclick()
+
+
+
