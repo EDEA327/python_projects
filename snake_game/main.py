bd0218843dbce625c8be3a32cf4461bf1374b75a
Erick <edeajob27@gmail.com>
2023-04-19 12:47:55 -0500

    serpiente avanza

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,27 +1,39 @@
 from turtle import Screen, Turtle
+import time
 
 # Create a new screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+# Turns off the animation.
+screen.tracer(0)
 
-# Crete the  initial snake
-# # Drawing white squares
+# Create the  initial snake
 # SQUARE_SIZE = 20
 SQUARE_COLOR = "white"
 SQUARE_SPACING = -20
 
-turtles = []
+segments = []
 
 # Create and add the three Turtle objects to the 'turtles' list
+# # Create a snake body
 for i in range(3):
-    t = Turtle(shape="square")
-    t.color(SQUARE_COLOR)
+    new_segment = Turtle(shape="square")
+    new_segment.color(SQUARE_COLOR)
     # t.shapesize(stretch_wid=SQUARE_SIZE, stretch_len=SQUARE_SIZE)
-    t.penup()
-    t.goto(x=i*SQUARE_SPACING, y=0)
-    turtles.append(t)
+    new_segment.penup()
+    new_segment.goto(x=i * SQUARE_SPACING, y=0)
+    segments.append(new_segment)
+# Update the screen
+screen.update()
+# Start the game
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    for segment in segments:
+        segment.forward(20)
 
 # Keep the window open
 screen.exitonclick()
