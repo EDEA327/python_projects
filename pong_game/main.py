37b58ae948c8e2c97648e030f69587b9889aeb3d
Erick <edeajob27@gmail.com>
2023-04-21 10:52:47 -0500

    Creé la pantalla en pong_game

---
+++ pong_game/main.py
@@ -0,0 +1,10 @@
+from turtle import Screen
+
+# Setting up the screen
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.title("Pong Game")
+screen.bgcolor("black")
+
+# Keep the window open
+screen.mainloop()
