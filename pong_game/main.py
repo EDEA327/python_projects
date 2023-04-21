a081ecf0a9e6afcf350609fccbfc9c443c7515dc
Erick <edeajob27@gmail.com>
2023-04-21 12:05:15 -0500

    Agregué tipado estatico en pong_game

--- pong_game/main.py
+++ pong_game/main.py
@@ -2,19 +2,21 @@ from turtle import Screen
 from paddle import Paddle
 
 # Setting up the screen
-screen = Screen()
+screen: Screen = Screen()
 screen.setup(width=800, height=600)
 screen.title("Pong Game")
 screen.bgcolor("black")
 
 # Creating the paddle
-paddle = Paddle()
+paddle: Paddle = Paddle()
 
 # Event listeners
 screen.listen()
+
 # I used lambda to be able to define a function without arguments, which is what the onkeypress() method needs
 screen.onkeypress(lambda: paddle.move("up"), "Up")
 screen.onkeypress(lambda: paddle.move("down"), "Down")
 
 # Keep the window open
 screen.mainloop()
+
--- pong_game/paddle.py
+++ pong_game/paddle.py
@@ -1,24 +1,57 @@
 from turtle import Turtle
 
+from typing import Tuple, List
+
+POSITIONS: List[Tuple[int, int]] = [(350, 0), (-350, 0)]
+
 
 class Paddle(Turtle):
-    def __init__(self):
-        super().__init__()
-        self.penup()
-        self.goto(x=350, y=0)
-        self.shape("square")
-        self.shapesize(stretch_wid=5, stretch_len=1)
-        self.color("white")
+    """
+    A class that represents a paddle object in a game of Pong.
 
-    def move(self, direction):
-        max_y = 250
-        min_y = -250
-        current_y = self.ycor()
+    Attributes:
+        None
+
+    Methods:
+        __init__(): Initializes the Paddle object.
+        create(): Creates the Paddle object.
+        move(direction: str): Moves the Paddle object up or down.
+    """
+
+    def __init__(self) -> None:
+        """
+        Initializes the Paddle object.
+        """
+        super().__init__()
+        self.create()
+
+    def create(self) -> None:
+        """
+        Creates the Paddle object.
+        """
+        for position in POSITIONS:
+            self.penup()
+            self.goto(position)
+            self.shape("square")
+            self.shapesize(stretch_wid=5, stretch_len=1)
+            self.color("white")
+
+    def move(self, direction: str) -> None:
+        """
+        Moves the Paddle object up or down.
+
+        Parameters:
+        direction (str): The direction in which to move the Paddle object. Can be either 'up' or 'down'.
+
+        Returns:
+        None
+        """
+        max_y: int = 250
+        min_y: int = -250
+        current_y: float = self.ycor()
+
         if direction == 'up' and current_y < max_y:
             self.sety(current_y + 20)
         elif direction == 'down' and current_y > min_y:
             self.sety(current_y - 20)
-
-
-
 
