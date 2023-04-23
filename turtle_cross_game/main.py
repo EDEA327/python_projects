01c655f58222526ab0851dcd36fccf26b0d41d11
Erick <edeajob27@gmail.com>
2023-04-22 19:31:28 -0500

    Primer commit en turtle cross

---
+++ turtle_cross_game/main.py
@@ -0,0 +1,8 @@
+from turtle import Screen
+
+# Set up the screen
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("white")
+screen.title("Turtle Crossing Street")
+screen.mainloop()
