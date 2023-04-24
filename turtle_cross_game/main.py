9423d2e1a85a4b212612758c9b9696f886777485
Erick <edeajob27@gmail.com>
2023-04-24 16:04:25 -0500

    Funcionalidad inicial de la clase Player en turtle cross

--- turtle_cross_game/main.py
+++ turtle_cross_game/main.py
@@ -1,8 +1,33 @@
+import time
 from turtle import Screen
 
-# Set up the screen
-screen = Screen()
-screen.setup(width=800, height=600)
-screen.bgcolor("white")
-screen.title("Turtle Crossing Street")
-screen.mainloop()
+from player import Player
+
+
+def main():
+    # Set up the screen
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.bgcolor("white")
+    screen.title("Turtle Crossing Street")
+    screen.tracer(0)
+
+    # Creates a player
+    player = Player()
+
+    # Event listeners
+    screen.listen()
+    screen.onkeypress(player.move, "Up")
+
+    # Game loop
+    game_on = True
+    while game_on:
+        time.sleep(0.1)
+        screen.update()
+
+    # Keep the screen open
+    screen.mainloop()
+
+
+if __name__ == "__main__":
+    main()
--- turtle_cross_game/player.py
+++ turtle_cross_game/player.py
@@ -1,7 +1,22 @@
+from turtle import Turtle
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.setheading(90)
+        self.penup()
+        self.goto(x=0, y=-280)
+
+    def move(self):
+        self.forward(10)
+
+
+
+
+
