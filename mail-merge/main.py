e8b5d778c819e04ad0eccace1aae184de79b7b3e
Erick <edeajob27@gmail.com>
2023-04-25 15:47:39 -0500

    Creé las cartas para los invitados en mail-merge v2

--- mail-merge/main.py
+++ mail-merge/main.py
@@ -1,4 +1,4 @@
-# TODO: Create a letter using starting_letter.txt
+
 def main():
 
     # for each name in invited_names.txt
@@ -17,9 +17,6 @@ def main():
 if __name__ == '__main__':
     main()
 
-
-
-    
 # Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
 # Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
 # Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
