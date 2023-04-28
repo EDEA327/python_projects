8a44307dc2949f6c8fb752dba932c27e0057784a
Erick <edeajob27@gmail.com>
2023-04-28 07:34:52 -0500

    reto de las ardillas en us_game

--- us_states_game/main.py
+++ us_states_game/main.py
@@ -22,3 +22,20 @@ import pandas as pd
 # data = pd.DataFrame(data_dict)
 # data.to_csv("student_data.csv")
 
+data = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+primary_color = data["Primary Fur Color"]
+gray = len(data[primary_color == "Gray"])
+cinnamon = len(data[primary_color == "Cinnamon"])
+black = len(data[primary_color == "Black"])
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray, cinnamon, black]
+}
+
+df = pd.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
+
+
+
+
