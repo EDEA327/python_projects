3dcb512efe2d80d4fa292dce6a624eb348a28b6e
Erick <edeajob27@gmail.com>
2023-05-30 09:57:46 -0500

    Cree el mecanismo de pomodoro/descanso en pomodoro

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -1,3 +1,4 @@
+import math
 import tkinter as tk
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -9,15 +10,31 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break = SHORT_BREAK_MIN * 60
+    long_break = LONG_BREAK_MIN * 60
 
+    if reps % 8 == 0:
+        count_down(long_break)
+    elif reps % 2 == 0:
+        count_down(short_break)
+    else:
+        count_down(work_sec)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
-    canvas.itemconfig(timer_text, text=count)
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
 
@@ -37,7 +54,7 @@ timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT
 canvas.grid(column=1, row=1)
 count_down(5)
 
-start_btn = tk.Button(text="Start", highlightthickness=0)
+start_btn = tk.Button(text="Start", highlightthickness=0, command=start_timer)
 start_btn.grid(column=0, row=3)
 
 reset_btn = tk.Button(text="Reset", highlightthickness=0)
