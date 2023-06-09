9318d3c791542f5f469c4595f9c7b8e9a5a78615
Erick <edeajob27@gmail.com>
2023-06-09 15:03:34 -0500

    Proyecto flashcard v1

--- flash-card/main.py
+++ flash-card/main.py
@@ -6,26 +6,56 @@ import pandas as pd
 
 # Constants
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
 
 
 # Code
 def new_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = choice(lang_data_list)
-    canvas.itemconfig(word, text=current_card["English"])
+    canvas.itemconfig(word, text=current_card["English"], fill="black")
+    canvas.itemconfig(language, text="English", fill="black")
+    canvas.itemconfig(card_bg, image=canvas_front_img)
+    window.after(3000, func=flip_card)
 
 
+def flip_card():
+    canvas.itemconfig(language, text="Español", fill="white")
+    canvas.itemconfig(word, text=current_card["Español"], fill="white")
+    canvas.itemconfig(card_bg, image=canvas_back_img)
+
+
+def is_known():
+    lang_data_list.remove(current_card)
+    data = pd.DataFrame(lang_data_list)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    new_card()
+
+
+# Extract data from .csv file
+try:
+    lang_data = pd.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    lang_data = pd.read_csv("data/1000 words in english - Hoja 1.csv")
+    lang_data_list = lang_data.to_dict(orient="records")
+else:
+    lang_data_list = lang_data.to_dict(orient="records")
+# print(lang_data_list)
 # Window Setup
 window = Tk()
 window.title("Flash Card")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
 canvas = Canvas(window, width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas_front_img = PhotoImage(file="images/card_front.png")
 canvas_back_img = PhotoImage(file="images/card_back.png")
-canvas.create_image(400, 263, image=canvas_front_img)
+card_bg = canvas.create_image(400, 263, image=canvas_front_img)
 canvas.grid(column=0, row=0, columnspan=2)
 
-language = canvas.create_text(400, 150, text="English", font=("Arial", 40, "italic"))
+language = canvas.create_text(400, 150, text="Language", font=("Arial", 40, "italic"))
 word = canvas.create_text(400, 263, text="Word", font=("Arial", 60, "bold"))
 
 wrong_img = PhotoImage(file="images/wrong.png")
@@ -33,12 +63,9 @@ wrong_btn = Button(image=wrong_img, highlightthickness=0, command=new_card)
 wrong_btn.grid(column=0, row=1)
 
 right_img = PhotoImage(file="images/right.png")
-right_btn = Button(image=right_img, highlightthickness=0, command=new_card)
+right_btn = Button(image=right_img, highlightthickness=0, command=is_known)
 right_btn.grid(column=1, row=1)
 
-# # Extract data from .csv file
-lang_data = pd.read_csv("data/1000 words in english - Hoja 1.csv")
-lang_data_list = lang_data.to_dict(orient="records")
-# print(lang_data_list)
+new_card()
 
 window.mainloop()
